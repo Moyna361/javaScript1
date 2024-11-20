4a122bcd6d8ff5b4caa3ee240afe0dc04ed6ff12
Moyna361 <devd22492@gmail.com>
2024-11-20 17:12:19 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,92 @@
+// var student = ["Moyna", "Suchitra", "Mariyam", "Humaira", "Sumi", "Jhorna"];
+// console.log(student);
+// console.log(student.length);
+// const studentArrayIndex = student.indexOf(Moyna)
+// console.log(studentArrayIndex);
+// var FindstudentIndex = student[3]
+
+
+// student[2] = "khadija"
+// console.log(student);
+
+// student.pop()
+
+// student.shift()
+// console.log(student);
+
+console.log(5 > 6);
+console.log(5 < 6);
+console.log(5 == 6);
+console.log(5 != 6);
+
+var jobHolder = true;
+var saveMoney = 15000;
+var hasFlat = false;
+
+if(jobHolder == true && saveMoney > 18000) {
+    console.log("yes");
+} else {
+    console.log("no");
+    
+}
+if(jobHolder == true && saveMoney > 18000 && hasFlat == true) {
+    console.log("yes");
+    
+    
+} else {
+    console.log("no");
+    
+}
+if((jobHolder == false && saveMoney > 10000 && hasFlat != true) && jobHolder == true) {
+    console.log("yes");
+    
+    
+} else {
+    console.log("no");
+    
+}
+
+var student = true;
+var roll = 121;
+var data = false;
+var cgpa = 9;
+
+if(student == false) {
+    if(cgpa == 9) {
+        console.log("isPass");
+        
+    } else {
+        console.log("isFail");
+        
+    }
+    
+}
+
+
+var Tourdestination = ["soudi arab", "Mokka", "modina", "filistine", "turusko"];
+console.log(Tourdestination);
+
+if(Tourdestination[4] = "modina") {
+    console.log("ghurte jabo");
+    
+} else if (Tourdestination[2] = "Mokka") {
+    console.log("ghute jabo mokka");
+    
+} else if (Tourdestination[3] = "Mokka") {
+    console.log("ghute jabo filistine");
+    
+} else {
+    console.log("kothu jabo na");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
